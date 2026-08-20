6b182fcf590a1aad68ca1572b5c8ad8f13bf10ad
Meg-Diom <d.ngum1@alustudent.com>
2026-08-20 05:51:21 +0400

    Validating the user's password

---
+++ quests/quest_12_two-path_cave.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+password = "**password26##"
+
+passwd = input("Enter your password: ")
+
+if passwd == password:
+    print("Access Granted")
+else:
+    print("Access Denied")
