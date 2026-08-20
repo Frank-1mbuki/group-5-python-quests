19eff6c46980aabe22b627ba929111a963608561
Meg-Diom <d.ngum1@alustudent.com>
2026-08-20 04:58:57 +0400

    Displaying a message on the screen

--- quests/quest_01_first_spell.py
+++ quests/quest_01_first_spell.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 
-print("{}".format("Welcome fellow adventurer! The road ahead is unknown, the dangers are many, and glory awaits those brave enough to seek it")
+print("{}".format("Welcome fellow adventurer! The road ahead is unknown, the dangers are many, and glory awaits those brave enough to seek it"))
 
