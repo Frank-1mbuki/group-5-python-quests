7e8677c06c0c02b7e76df246f78df5c5b4e2fd1e
Meg-Diom <d.ngum1@alustudent.com>
2026-08-20 18:25:51 +0400

    A nested riddle

---
+++ quests/quest_15_nested_riddle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+option = input("Hi! Are you going right or left? ")
+if option == "left":
+    option_2 = input("Will you swim or you will wait? ")
+    if option_2 == "swim":
+        print("Congratulations! You found a treasure")
+    else:
+        print("Try again")
+
+else:
+    print("Try again")
