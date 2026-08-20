0369fb9420a3a015eab8189e394cb8139ba76be4
Meg-Diom <d.ngum1@alustudent.com>
2026-08-20 18:55:44 +0400

    function to display the area of a rectangle4

---
+++ quests/quest_23_oracle_vision.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+
+print(f"area: {calculate_area(8, 6)}")
+
+print(f"area: {calculate_area(9, 7)}")
