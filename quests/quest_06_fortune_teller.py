368bdb7e3cb21aa6243384294d1f16485a56fd3b
Meg-Diom <d.ngum1@alustudent.com>
2026-08-20 05:25:28 +0400

    Prompting the user for input and returning the user some output

---
+++ quests/quest_06_fortune_teller.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+name = input("What is your name: ")
+
+quest = int(input("What is your quest number: "))
+
+print(f"Your name is {name} and your quest number is {quest}")
