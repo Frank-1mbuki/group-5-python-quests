e1026f50e904d287823632c8770e42d069b65fa6
Meg-Diom <d.ngum1@alustudent.com>
2026-08-20 18:41:10 +0400

    Checking the parity of numbers in a range

---
+++ quests/quest_09_greedy_goblin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+gold_pieces = 27
+
+number_of_gold_pieces_per_friend = print(f"Each friend gets {gold_pieces//4} gold pieces")
+
+remainder = print(f"Globin gets {gold_pieces % 4} gold pieces")
---
+++ quests/quest_20_even_number.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+for number in range(20):
+    if number % 2 == 0:
+        print("This is an even number")
+    else:
+        print("This is an odd number")
