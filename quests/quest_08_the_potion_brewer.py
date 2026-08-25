730174b2beeef329858a41654d31fef0090a739b
Monique237 <m.nchiefieh@alustudent.com>
2026-08-25 14:46:51 -0700

    quest_08_the_potion_brewer.py

---
+++ quests/quest_08_the_potion_brewer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+dragon_scales = 3
+dragon_scale_price = 10
+
+elf_tears = 5
+elf_tear_price = 3
+
+dragon_scales_cost = dragon_scales * dragon_scale_price
+elf_tears_cost = elf_tears * elf_tear_price
+
+total_cost = dragon_scales_cost + elf_tears_cost
+
+print(f"Total cost: {total_cost} gold")
