cb31cb83e0ed6de92f54d02b7ac7f2853a9e590b
Frank-1mbuki <frankimbuki03@gmail.com>
2026-08-26 11:10:26 +0400

    Quest 28: The Adventure Begins

---
+++ quests/quest_28_adventure_begins.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def start():
+    choice = input("Path A or Path B? (A/B): ").strip().upper()
+    win() if choice == 'A' else lose()
+
+def win(): 
+    print("You found the gold. [Ending 1: Win]")
+
+def lose(): 
+    print("You fell in a pit. [Ending 2: Lose]")
+
+if __name__ == "__main__":
+    start()
