e5b1eacb928220a162eb24360363bf132caa32d0
Frank Imbuki <frankimbuki03@gmail.com>
2026-08-27 09:37:19 +0400

    Add user input for birth year and age calculation
    Prompt user for birth year and calculate age.

--- quests/quest_07_magic_number_converter.py
+++ quests/quest_07_magic_number_converter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+# Prompt user for input and cast to integer.
 birth_year=int(input("What is your birth year? "))
 
 from datetime import datetime
 current_year=datetime.now().year
-
+#Calculate the users age
 user_age=current_year-birth_year
 print(f"{user_age}")
