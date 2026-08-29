4f492732387cbada20b89a0b7c6f983956585beb
Frank Imbuki <frankimbuki03@gmail.com>
2026-08-29 10:13:08 +0400

    Update quest to take user input for name, city, and year

--- quests/quest_04_town_crier.py
+++ quests/quest_04_town_crier.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-# Asks the user for their name, city and year and prints a welcome message.
-city_name="Pamplemousses"
-current_year=2026
-your_name="Frank Imbuki"
+
+# Asks the user for their name, city, and year and prints a welcome message.
+city_name = input("Enter the city name: ")
+current_year = input("Enter the current year: ")
+your_name = input("Enter your name: ")
+
 print(f"Welcome to {city_name}! The year is {current_year}, and our newest resident is {your_name}.")
