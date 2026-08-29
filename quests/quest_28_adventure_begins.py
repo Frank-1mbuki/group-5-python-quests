6e2dea93a0516482f0b909f6cc42702ea53b094c
Frank Imbuki <frankimbuki03@gmail.com>
2026-08-29 10:36:02 +0400

    Refactor win and lose functions for clarity

--- quests/quest_28_adventure_begins.py
+++ quests/quest_28_adventure_begins.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
+
 def start():
     choice = input("Path A or Path B? (A/B): ").strip().upper()
     win() if choice == 'A' else lose()
 
-def win(): 
+
+def win():
     print("You found the gold. [Ending 1: Win]")
 
-def lose(): 
+
+def lose():
     print("You fell in a pit. [Ending 2: Lose]")
+
 
 if __name__ == "__main__":
     start()
