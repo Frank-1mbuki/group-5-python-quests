c3c1b2d67edf06b6117b0af8974a2ca31a29953e
Frank Imbuki <frankimbuki03@gmail.com>
2026-08-29 10:26:35 +0400

    Refactor guessing loop for clarity and structure

--- quests/quest_18_loop_of_riddles.py
+++ quests/quest_18_loop_of_riddles.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+
+# Loop continuously until the user guesses the correct number.
 while True:
-    secret_number=int(input("Guess a number between 0 and 10: " ))
-    if secret_number==7:
+    secret_number = int(input("Guess a number between 0 and 10: "))
+    
+    # Exit the loop immediately if the winning condition is met.
+    if secret_number == 7:
         print("You nailed it!")
         break
+        
+    # Provide feedback for incorrect guesses before looping again.
     print("Nice try! Give it another go.")
