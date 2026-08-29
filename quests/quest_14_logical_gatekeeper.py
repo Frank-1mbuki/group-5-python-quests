04c8d1292c38466b858db5b45f3db320d22c5113
Frank Imbuki <frankimbuki03@gmail.com>
2026-08-29 10:23:13 +0400

    Refactor variable names for guest age and coins

--- quests/quest_14_logical_gatekeeper.py
+++ quests/quest_14_logical_gatekeeper.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-GUEST_AGE=int(input("How old are you? "))
-NUMBER_OF_COINS=int(input("How many gold coins do you have? "))
 
-#requires guests to be 18+ AND have 20+ gold coins
-if GUEST_AGE >= 18 and NUMBER_OF_COINS >= 20:
+guest_age = int(input("How old are you? "))
+number_of_coins = int(input("How many gold coins do you have? "))
+
+# Requires guests to be 18+ AND have 20+ gold coins
+if guest_age >= 18 and number_of_coins >= 20:
     print("You may enter!")
 else:
     print("You do not meet the requirements. You may not enter!")
+    
